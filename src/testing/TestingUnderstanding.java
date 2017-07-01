da2dbb8400707c2321adf4cfd45df2946e405365
Your Name <nikhilreddy24@hotmail.com>
2017-07-01 12:59:40 +1200

    Just initilazed and started web parasing

---
+++ src/testing/TestingUnderstanding.java
@@ -0,0 +1,24 @@
+package testing;
+
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class TestingUnderstanding {
+	public static void main(String args[]) {
+		
+		try {
+			Document doc  = Jsoup.connect("http://basketball.realgm.com/nba/stats").userAgent("mozilla/17.0").get();
+			Elements temp = doc.select("div.overall-leader");
+			for(Element Player: temp) {
+				System.out.println(""+Player.getElementsByTag("a").text());
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
