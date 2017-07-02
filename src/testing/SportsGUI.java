21e491abc208a2ee45097043ce7cea15b80665e7
Your Name <nikhilreddy24@hotmail.com>
2017-07-02 23:24:28 +1200

    Updated the Gui App to make the tables have the correct column widths
    Also Added the JtextFrame and Buttons for updating the infromation

--- src/testing/SportsGUI.java
+++ src/testing/SportsGUI.java
@@ -1,50 +1,66 @@
 package testing;
 
-import java.awt.Dimension;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 import javax.swing.JTable;
 import javax.swing.JTextArea;
 
+
 public class SportsGUI extends JPanel {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	String[] coulmnNames = {"Name","Pos","Team","GP","GS","AST","TRB","PPG"};
+	String[] coulmnNames = {"Name","Pos","Age","Team","GP","GS","AST","TRB","PPG"};
 	TestingUnderstanding A = new TestingUnderstanding();
 	private JButton _updateBtn;
 	private JTextArea _outputLog;
 	private JTable _table;
 	private JScrollPane scrollPane;
-	private final JSplitPane splitPane;  // split the window in top and bottom
 	private final JPanel topPanel;       // container panel for the top
-	private final JPanel bottomPanel;    // container panel for the bottom
-	public SportsGUI() {
+	public SportsGUI() throws IOException {
 		_updateBtn = new JButton("UpdateButton");
-		_outputLog = new JTextArea();
+		_outputLog = new JTextArea(1,1);
 		_outputLog.setEditable(false);
 		_table = new JTable(A.GetPlayerData(),coulmnNames);
 		_table.setFillsViewportHeight(true);
+		this.SetColumnSizes(_table);
+		BufferedImage myPicture;
 		scrollPane = new JScrollPane(_table);
-		splitPane = new JSplitPane();
 		topPanel = new JPanel();
-		bottomPanel = new JPanel();
+		JPanel RightPanel = new JPanel(new BorderLayout());
+		RightPanel.setSize(100, 100);
 		topPanel.add(scrollPane);
-		bottomPanel.add(_updateBtn);
-		splitPane.setLeftComponent(bottomPanel);
-		splitPane.setRightComponent(topPanel);
-		add(splitPane);
-		add(_outputLog);
+		RightPanel.add(_updateBtn,BorderLayout.NORTH);
+		RightPanel.add(_outputLog,BorderLayout.SOUTH);
+		_outputLog.append("Lebron Is Great");
+		try {
+			myPicture = ImageIO.read(new File("C:/Users/Nikhil/Desktop/thumbnails/Life of pablo.jpg"));
+			JLabel picLabel = new JLabel(new ImageIcon(myPicture));
+			RightPanel.add(picLabel,BorderLayout.CENTER);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		add(topPanel);
+		add(RightPanel);
 	}
 
-	private static void createAndShowGUI() {
-		
+	private static void createAndShowGUI() throws IOException {
+
 		JFrame frame = new JFrame("SportsStats");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JComponent newContentPane = new SportsGUI();
@@ -54,10 +70,27 @@ public class SportsGUI extends JPanel {
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 	}
+	public void SetColumnSizes(JTable _table) {
+		_table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		_table.getColumnModel().getColumn(0).setPreferredWidth(140);
+		_table.getColumnModel().getColumn(1).setPreferredWidth(40);
+		_table.getColumnModel().getColumn(2).setPreferredWidth(40);
+		_table.getColumnModel().getColumn(3).setPreferredWidth(40);
+		_table.getColumnModel().getColumn(4).setPreferredWidth(40);
+		_table.getColumnModel().getColumn(5).setPreferredWidth(40);
+		_table.getColumnModel().getColumn(6).setPreferredWidth(40);
+		_table.getColumnModel().getColumn(7).setPreferredWidth(40);
+		_table.getColumnModel().getColumn(8).setPreferredWidth(40);
+	}
 	public static void main(String []args) {
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
-				createAndShowGUI();
+				try {
+					createAndShowGUI();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 		});
 
